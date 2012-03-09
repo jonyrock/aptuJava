d267e78f96ce998a186a691f77fac23045b37597
JJXX <jjxx@ubuntu.ubuntu-domain>
2012-03-09 20:58:41 +0400

    constant imp

--- src/ru/spbau/velikiy/task3/core/OperationConstant.java
+++ src/ru/spbau/velikiy/task3/core/OperationConstant.java
@@ -1,37 +1,40 @@
 package spbau.velikiy.task3.core;
 
 
-public class OperationConstant extends Tree{
-    
+public class OperationConstant extends Tree {
+
     private final int value;
 
     /**
-     * 
-     * Create constant from source string expression 
+     * Create constant from source string expression
      * and pointer in it
-     * 
-     * @param s source expression
+     *
+     * @param s       source expression
      * @param pointer position in expression
      */
-    public OperationConstant(char[] s, int pointer){
-        
-        this.value = 0;
+    public OperationConstant(char[] s, int pointer) {
 
-        //this.value.valueByString(s, pointer);
-        
-        
-        //this.value = 10;
+        int parsedValue = 0;
 
+        while (pointer < s.length) {
+            if (!Character.isDigit(s[pointer])) {
+                break;
+            }
+            parsedValue = parsedValue * 10 + Character.digit(s[pointer], 10);
+            pointer++;
+        }
+        
+        value = parsedValue;        
 
     }
-        
-    
-    public OperationConstant(int value){
-        this.value = value;        
+
+
+    public OperationConstant(int value) {
+        this.value = value;
     }
-    
-    public int value(int xValue){
-        
+
+    public int value(int xValue) {
+
         return value;
 
     }
